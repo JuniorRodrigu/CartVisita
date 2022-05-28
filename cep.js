a92277605d0e9d452bda3a7bd9af85ac23516656
JuniorRodrigu <juniorrodrigues1717@gmail.com>
2022-05-28 20:59:07 -0300

    fim

---
+++ cep.js
@@ -0,0 +1,4 @@
+curl --location --request POST 'https://correios.contrateumdev.com.br/api/cep' \
+--data-raw '{
+    "cep" : "31160550"
+}'
--- main.py
+++
@@ -1,4 +0,0 @@
-from cgi import print_environ
-
-
-print("vamos nessa!!!!!") 
